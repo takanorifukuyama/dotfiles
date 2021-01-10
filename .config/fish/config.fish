7ca597a0fbb8a044f806908c306c001ab46f1115
Takanori Fukuyama <takanori.fukuyama@gmail.com>
2021-01-10 15:33:16 +0900

    :sparkles: init yarn, gitconfig

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,7 +3,7 @@ alias fc "nvim ~/.config/fish/config.fish"
 alias g "git"
 alias feature "git flow feature"
 alias ga "git add"
-alias gp "git push"
+alias gps "git push"
 alias gst "git status"
 alias gc "git commit -am"
 alias gcm "git commit -m"
@@ -17,15 +17,24 @@ alias ctx "kubectx"
 alias ns "kubens"
 alias val "kubeval"
 
-set -x PATH ~/.SDK/flutter/bin $PATH
+# rust
 set -x PATH ~/.cargo/bin $PATH
+
+# python
 set -x PATH ~/.pyenv/shims $PATH
-set -x PATH ~/.pub-cache/bin $PATH
-set GHQ_SELECTOR peco
+
+# android studio
 set -x ANDROID_HOME ~/Library/Android/sdk
 set -x PATH $ANDROID_HOME/emulator/ $PATH
+set -x PATH $ANDROID_HOME/platform-tools $PATH
+
+# flutter
+set -x PATH ~/.SDK/flutter/bin $PATH
 set -x DART_SDK ~/.SDK/flutter/bin/cache/dart-sdk/bin
+set -x PATH ~/.pub-cache/bin $PATH
 set -x PATH $DART_SDK $PATH
+
+# nodejs
 set -x PATH ~/.nodebrew/current/bin $PATH
 
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish ; or true
@@ -34,6 +43,7 @@ test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shel
 # pyenv init - | source
 # pyenv virtualenv-init - | source
 
+set GHQ_SELECTOR peco
 function fish_user_key_bindings
   bind \cr peco_select_history # Bind for prco history to Ctrl+r
 end
@@ -45,5 +55,10 @@ set -x GOPATH ~/go
 set -x GO111MODULE on
 set -x PATH $GOBIN $PATH
 
+# apple silicon setting
 set -x PATH /opt/homebrew/bin $PATH
 set -x PATH ~/nvim-osx64/bin $PATH
+
+# Wasmer
+export WASMER_DIR="/Users/takanorifukuyama/.wasmer"
+[ -s "$WASMER_DIR/wasmer.sh" ] && source "$WASMER_DIR/wasmer.sh"
--- README.md
+++ README.md
@@ -5,3 +5,10 @@
 ```
 source dotfiles.sh
 ```
+
+following package required
+
+https://github.com/dandavison/delta
+```fish
+brew install git-delta
+```
