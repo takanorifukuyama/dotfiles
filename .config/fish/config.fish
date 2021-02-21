3521076c8bd7f85d45dc3b8239751d368f0cfec1
Takanori Fukuyama <takanori.fukuyama@gmail.com>
2021-02-21 16:14:13 +0900

    file separartion

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,29 +1,3 @@
-alias v "nvim"
-alias f "fvm flutter"
-alias fc "nvim ~/.config/fish/config.fish"
-alias nc "nvim ~/.config/nvim"
-alias g "git"
-alias feature "git flow feature"
-alias ga "git add"
-alias gps "git push"
-alias gpl "git pull"
-alias gst "git status"
-alias gc "git commit"
-alias gcm "git commit -m"
-alias gdd "git diff | delta --dark"
-alias gb "git branch"
-alias gs "git switch"
-alias cl clear
-alias ef "exec fish -l"
-alias ctl "kubectl"
-alias ctx "kubectx"
-alias ns "kubens"
-alias val "kubeval"
-alias code "code-insiders"
-alias d "docker-compose"
-
-alias bz "blitz"
-
 # rust
 set -x PATH ~/.cargo/bin $PATH
 
