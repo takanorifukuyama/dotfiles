421899bdf6a52743872533f0e6f6ecb39195085e
Takanori Fukuyama <takanori.fukuyama@gmail.com>
2022-01-03 13:27:49 +0900

    name changed

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -2,5 +2,10 @@
 
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.ideavimrc ~/.ideavimrc
-ln -sf ~/dotfiles/.config ~/.config
 ln -sf ~/dotfiles/.tigrc ~/.tigrc
+
+ln -sf ~/dotfiles/config/nvim ~/.config/nvim
+ln -sf ~/dotfiles/config/fish ~/.config/fish
+ln -sf ~/dotfiles/config/flutter ~/.config/flutter
+ln -sf ~/dotfiles/config/gh ~/.config/gh
+ln -sf ~/dotfiles/config/starship.toml ~/.config/starship.toml
