68e5f090fa45c772c33e41218afa1c604f5f1aee
Takanori Fukuyama <takanori.fukuyama@gmail.com>
2023-04-21 14:49:46 +0900

    wezterm fontsize zoom

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -26,11 +26,24 @@ wezterm.on(
     end
 )
 
+wezterm.on("zoom-in", function(window, pane)
+    local overrides = window:get_config_overrides() or {}
+    overrides.font_size = (overrides.font_size or 14) + 0.5
+    window:set_config_overrides(overrides)
+end)
+
+wezterm.on("zoom-out", function(window, pane)
+    local overrides = window:get_config_overrides() or {}
+    overrides.font_size = (overrides.font_size or 14) - 0.5
+    window:set_config_overrides(overrides)
+end)
+
 return {
   default_cursor_style = 'BlinkingBar',
-  font = wezterm.font('Source Code Pro', { weight = 'Medium' }),
+  font = wezterm.font('PlemolJP Console NF', { weight = 'Regular' }), -- Light, Regular, Medium, SemiBold, Bold
   -- window_background_opacity = 1,
-  font_size = 12,
+  font_size = 14,
+  audible_bell = "Disabled", -- bellを鳴らさない
   keys = {
     { key = 'd', mods = 'CMD', action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' } },
     { key = 'D', mods = 'CMD', action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' } },
@@ -41,5 +54,7 @@ return {
     {key = "DownArrow", mods = "ALT", action = wezterm.action {ActivatePaneDirection = "Down"}},
 
     {key = "u", mods = "CMD", action = wezterm.action {EmitEvent = "toggle-opacity"}},
+    {key = "e", mods = "CMD", action = wezterm.action {EmitEvent = "zoom-out"}},
+    {key = "E", mods = "CMD", action = wezterm.action {EmitEvent = "zoom-in"}},
   }
 }
